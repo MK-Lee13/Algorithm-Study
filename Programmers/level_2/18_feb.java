3fc484b7bff4685b39ef76433623ca33ca32b040
koreaaerospaceU <zx811@naver.com>
2020-10-20 02:27:04 +0900

    프로그래머스 레벨 2 피보나치 수

---
+++ Programmers/level_2/18_feb.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            arr[i] = (arr[i-1] % 1234567 + arr[i-2] % 1234567) % 1234567;
+        }
+        return arr[n];
+    }
+}
