646913c8d77e2eb606b74cc3e4d5651f9d4b86dc
koreaaerospaceU <zx811@naver.com>
2022-03-17 21:57:03 +0900

    프로그래머스 레벨 3 등굣길

---
+++ Programmers/level_3/14_road.py
@@ -0,0 +1,19 @@
+def solution(m, n, puddles):
+    answer = 0
+    p_map = make_map(m, n)
+    p_map[0][0] = 1
+    for i in range(0, n):
+        for j in range(0, m):
+            if [j + 1, i + 1] in puddles or [i, j] == [0, 0]:
+                continue
+            if i == 0:
+                p_map[i][j] = p_map[i][j - 1]
+            elif j == 0:
+                p_map[i][j] = p_map[i - 1][j]
+            else:
+                p_map[i][j] = p_map[i - 1][j] + p_map[i][j - 1]
+    return p_map[n - 1][m - 1] % 1000000007
+
+
+def make_map(m, n):
+    return [[0] * (m) for _ in range(n)]
